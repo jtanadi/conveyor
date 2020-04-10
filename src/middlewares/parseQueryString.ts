4f475c606d4d850d1955b62ca5415202cba38314
Jesen Tanadi <mail@jesentanadi.com>
2020-04-09 22:12:53 -0400

    initial commit

---
+++ src/middlewares/parseQueryString.ts
@@ -0,0 +1,34 @@
+import { Protocol, Response } from "restana"
+import url from "url"
+
+import { ExtendedRequest } from "../types"
+
+export default (
+  req: ExtendedRequest,
+  res: Response<Protocol.HTTPS>,
+  next: () => void
+) => {
+  const qs = url.parse(req.url, true).query
+
+  if (Array.isArray(qs.out) || Array.isArray(qs.download)) {
+    throw new Error(
+      "conveyor only accepts one 'out' param and one 'download' param"
+    )
+  }
+
+  let outFileType: string
+  if (!qs.out) {
+    outFileType = "png"
+  } else if (/^jpe?g$/i.test(qs.out)) {
+    outFileType = "jpg"
+  } else if (/^tiff?$/i.test(qs.out)) {
+    outFileType = "tif"
+  } else {
+    outFileType = qs.out.toLowerCase()
+  }
+
+  req.outFileType = outFileType
+  req.clientDownload = qs.download?.toLowerCase() === "true" ? true : false
+
+  next()
+}
