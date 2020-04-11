05cd8d41910bbea0ad3aaf095d3df7d7e5d3d28d
Jesen Tanadi <mail@jesentanadi.com>
2020-04-11 11:08:09 -0400

    add eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:prettier/recommended",
     "prettier/@typescript-eslint",
@@ -23,16 +22,15 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "prettier"],
   rules: {
-    "react/prop-types": 0,
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/explicit-function-return-type": 0,
   },
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
+      files: ["*.ts"],
       rules: {
         "@typescript-eslint/explicit-function-return-type": ["error"],
       },
