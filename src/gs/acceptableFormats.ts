91c80c08d6612e1d3bb07adc22ff8b385b21e696
Jesen Tanadi <mail@jesentanadi.com>
2020-04-13 16:17:50 -0400

    initial commit: GhostScript adaptor

---
+++ src/gs/acceptableFormats.ts
@@ -0,0 +1,11 @@
+// Mapping of common format names and ghostscript "devices"
+// https://www.ghostscript.com/doc/9.22/Devices.htm
+const acceptableFormats = new Map()
+acceptableFormats.set("png", "png256")
+acceptableFormats.set("jpg", "jpeg")
+acceptableFormats.set("tif", "tiff24nc")
+acceptableFormats.set("ps", "ps2write")
+acceptableFormats.set("eps", "eps2write")
+acceptableFormats.set("pdf", "pdf")
+
+export default acceptableFormats
