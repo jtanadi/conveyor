47d21c7efcb4bd03b36ee7fdc23eaebc76ae5c89
Jesen Tanadi <mail@jesentanadi.com>
2020-04-17 11:46:43 -0400

    get measurements to calculate resolution; use promise instead of async/await

--- src/utils/processQueue.ts
+++ src/utils/processQueue.ts
@@ -2,6 +2,8 @@ import axios from "axios"
 import path from "path"
 
 import cleanup from "./cleanup"
+import measurePDF from "./measurePDF"
+import getResolution from "./getResolution"
 import gs from "../gs"
 import queue from "./queue"
 import uploadToS3 from "./uploadToS3"
@@ -12,33 +14,33 @@ type PostData = {
   forwardData?: string
 }
 
-const outputResolution = 150
-
 export default async (): Promise<void> => {
   const task = queue.dequeue()
 
   const { pingback, filename, inputFilePath, outputDir, outFileType } = task
 
-  /* eslint-disable no-useless-catch */
-  try {
-    const outputFilePath = path.join(outputDir, "page")
-    await gs.convert(
-      inputFilePath,
-      outputFilePath,
-      outFileType,
-      outputResolution
-    )
+  // Get page measurements and calculate resolution based on max sizes
+  // Measurements are arrays of [width, height]
+  const measurements: number[][] = await measurePDF(inputFilePath)
+  const outputResolution = getResolution(measurements)
 
-    const files = await uploadToS3(outputDir, filename, outFileType)
+  const outputFilePath = path.join(outputDir, "page")
+  gs.convert(inputFilePath, outputFilePath, outFileType, outputResolution)
+    .then(() => {
+      return uploadToS3(outputDir, filename, outFileType)
+    })
+    .then((files) => {
+      const postData: PostData = { s3Dir: filename, files }
+      if (task.forwardData) {
+        postData.forwardData = task.forwardData
+      }
 
-    const postData: PostData = { s3Dir: filename, files }
-    if (task.forwardData) {
-      postData.forwardData = task.forwardData
-    }
-
-    axios.post(pingback, postData)
-    cleanup(outputDir)
-  } catch (e) {
-    throw e
-  }
+      axios.post(pingback, postData)
+    })
+    .then(() => {
+      cleanup(outputDir)
+    })
+    .catch((err) => {
+      throw err
+    })
 }
