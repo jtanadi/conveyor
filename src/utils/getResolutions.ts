a40758ec37fc40bc7e51b181e63c054d18fff3ca
Jesen Tanadi <mail@jesentanadi.com>
2020-04-19 09:47:06 -0400

    clean up; fix resolution calculation based on orientation

--- src/utils/getResolutions.ts
+++ src/utils/getResolutions.ts
@@ -5,24 +5,13 @@ const MAX_PIXELS = {
 }
 
 export default (dims: number[][]): number[] => {
-  let maxWidth = 0
-  let maxHeight = 0
-
-  for (const dim of dims) {
-    if (dim[0] > maxWidth) {
-      maxWidth = dim[0]
-    }
-
-    if (dim[1] > maxHeight) {
-      maxHeight = dim[1]
-    }
-  }
-
   return dims.map((dim: number[]) => {
     if (dim[0] < dim[1]) {
-      return Math.round(MAX_PIXELS.WIDTH / dim[0])
-    } else {
+      // Portrait, scale to height
       return Math.round(MAX_PIXELS.HEIGHT / dim[1])
+    } else {
+      // Landscape or square, scale to width
+      return Math.round(MAX_PIXELS.WIDTH / dim[0])
     }
   })
 }
