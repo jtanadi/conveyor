22cea054a74db3f318c9597613d1ebfaf0bd6830
Jesen Tanadi <mail@jesentanadi.com>
2020-04-19 23:23:32 -0400

    instantiate new PDFParser every time we need to measure document

--- src/utils/measurePDF.ts
+++ src/utils/measurePDF.ts
@@ -1,11 +1,11 @@
 import PDFParser from "pdf2json"
 
-const pdfParser = new PDFParser()
-
 // Not sure why it's this
 const MAGIC_NUMBER = 4.5
 
 export default (pdfPath: string): Promise<number[][]> => {
+  const pdfParser = new PDFParser()
+
   pdfParser.loadPDF(pdfPath)
 
   return new Promise((resolve, reject) => {
