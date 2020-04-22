1b694643184a794d0c32da57b1d555e2bb519035
Jesen Tanadi <mail@jesentanadi.com>
2020-04-22 08:25:58 -0400

    use higher max res and 17in. for width and height

--- src/utils/getResolutions.ts
+++ src/utils/getResolutions.ts
@@ -1,17 +1,14 @@
-// Based on 17 x 11 @ 150 DPI
-const MAX_PIXELS = {
-  WIDTH: 2550,
-  HEIGHT: 1650,
-}
+// 17" @ 250 DPI
+const MAX_PIXELS = 17 * 250
 
 export default (dims: number[][]): number[] => {
   return dims.map((dim: number[]) => {
     if (dim[0] < dim[1]) {
       // Portrait, scale to height
-      return Math.round(MAX_PIXELS.HEIGHT / dim[1])
+      return Math.round(MAX_PIXELS / dim[1])
     } else {
       // Landscape or square, scale to width
-      return Math.round(MAX_PIXELS.WIDTH / dim[0])
+      return Math.round(MAX_PIXELS / dim[0])
     }
   })
 }
