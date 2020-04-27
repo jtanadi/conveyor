93d6b65a7f09f0f4444b901677398517d0efe0e8
Jesen Tanadi <mail@jesentanadi.com>
2020-04-27 07:58:22 -0400

    convert to async/await

--- src/utils/postPingback.ts
+++ src/utils/postPingback.ts
@@ -1,14 +1,17 @@
 import axios from "axios"
 import { PingbackData } from "../types"
 
-export default (pingbackAddress: string, data: string | PingbackData): void => {
+export default async (
+  pingbackAddress: string,
+  data: string | PingbackData
+): Promise<void> => {
   if (process.env.NODE_ENV !== "production" && process.env.LOCAL) {
     console.log("\x1b[33m%s\x1b[0m", "* Data to be sent to pingback below *")
     console.log("\x1b[33m%s\x1b[0m", "* No POST made to pingback URL *")
     console.log("DATA", data, "\n")
   } else {
     try {
-      axios.post(pingbackAddress, data)
+      await axios.post(pingbackAddress, data)
     } catch (e) {
       console.error(e)
     }
